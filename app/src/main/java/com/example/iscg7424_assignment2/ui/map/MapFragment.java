50b18cbe8a0d474389ca1e3c8e3bf09382c486ba
kamho99 <kamho99@gmail.com>
2020-10-24 23:12:58 +1300

    Update

--- app/src/main/java/com/example/iscg7424_assignment2/ui/map/MapFragment.java
+++ app/src/main/java/com/example/iscg7424_assignment2/ui/map/MapFragment.java
@@ -1,38 +1,29 @@
 package com.example.iscg7424_assignment2.ui.map;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.example.iscg7424_assignment2.R;
 
 public class MapFragment extends Fragment {
 
-    private MapViewModel mViewModel;
+    private MapViewModel mapViewModel;
 
-    public static MapFragment newInstance() {
-        return new MapFragment();
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        mapViewModel =
+                ViewModelProviders.of(this).get(MapViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+
+        return root;
     }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(MapViewModel.class);
-        // TODO: Use the ViewModel
-    }
-
 }
